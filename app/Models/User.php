aaec4120d1dc7708f527ed09ea00edccdb5511dd
Helen orejuela mancilla <135240673+Helen10mancilla@users.noreply.github.com>
2024-10-31 09:32:45 -0500

    se agregan los archivos

---
+++ app/Models/User.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable, HasRoles; // Incluye el trait HasRoles para la gestión de roles
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+
+    /**
+     * Asigna un rol al usuario.
+     *
+     * @param string $role
+     * @return void
+     */
+    public function asignarRol($role)
+    {
+        // Verifica si el rol existe antes de asignarlo
+        if (! $this->hasRole($role)) {
+            $this->assignRole($role); // Usa assignRole del paquete Spatie
+        }
+    }
+}
+
